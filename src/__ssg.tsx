e81cfab3799c5076146ef7a79bc054dc304a1499
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-15 07:03:24 +0900

    Improved <Document> boilerplate loosely based on CRA

--- src/__ssg.tsx
+++ src/__ssg.tsx
@@ -14,15 +14,22 @@ function Document({ route }: { route: React.ReactElement }) {
 	return (
 		<html lang="en">
 			<head>
-				<meta charSet="UTF-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>Document</title>
+				<meta charSet="utf-8" />
+				{/* TODO: If we build to build then it make sense to nest public for
+				static assets. Alternatively we can build to public and nest static
+				for static assets. Either way, favicon.icon is a little confusing
+				because it doesn’t make clear where static assets go. */}
+				<link rel="icon" href="favicon.ico" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="description" content="Web site created using create-react-app" />
+				<title>SSG React App</title>
 				{/* TODO: This assumes style.css never changes and or doesn’t support
 				other CSS files. If we use a compositional API this is basically a
 				solved problem. */}
 				<link rel="stylesheet" href="style.css" />
 			</head>
 			<body>
+				<noscript>You need to enable JavaScript to run this app.</noscript>
 				<div id="root">{route}</div>
 				{/* TODO: This assumes script.js never changes and or doesn’t support
 				other JS files. If we use a compositional API this is basically a
