b5384c6158f1f7dbf0859eff320caa1dbd65c84c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-15 08:20:30 +0900

    Save changes before messing with routes

--- src/utils/handle.ts
+++ src/utils/handle.ts
@@ -1,9 +1,11 @@
+// Go-style error handling.
+//
 // https://dev.to/sobiodarlington/better-error-handling-with-async-await-2e5m
 export default async function handle<T>(promise: Promise<T>) {
 	try {
 		const data = await promise
 		return [data, undefined]
-	} catch (error) {
-		return await Promise.resolve([undefined, error])
+	} catch (err) {
+		return await Promise.resolve([undefined, err])
 	}
 }
