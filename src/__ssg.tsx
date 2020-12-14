af76b027376291352cd39b241a82109725e32935
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-15 07:26:36 +0900

    Compositional <Document> (closes #5)

--- src/__ssg.tsx
+++ src/__ssg.tsx
@@ -1,49 +1,15 @@
 import * as fs from "fs"
 import * as React from "react"
 import * as ReactDOMServer from "react-dom/server"
+import Document from "./document"
 import routes from "./routes"
 import { StaticRouter } from "react-router-dom"
-
-// TODO: Extract to a compositional API.
-// TODO: We probably need some high-level components like:
-//
-// - <Head>
-// - <Title> or <DocumentTitle>
-//
-function Document({ route }: { route: React.ReactElement }) {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				{/* TODO: If we build to build then it make sense to nest public for
-				static assets. Alternatively we can build to public and nest static
-				for static assets. Either way, favicon.icon is a little confusing
-				because it doesn’t make clear where static assets go. */}
-				<link rel="icon" href="favicon.ico" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="description" content="Web site created using create-react-app" />
-				<title>SSG React App</title>
-				{/* TODO: This assumes style.css never changes and or doesn’t support
-				other CSS files. If we use a compositional API this is basically a
-				solved problem. */}
-				<link rel="stylesheet" href="style.css" />
-			</head>
-			<body>
-				<noscript>You need to enable JavaScript to run this app.</noscript>
-				<div id="root">{route}</div>
-				{/* TODO: This assumes script.js never changes and or doesn’t support
-				other JS files. If we use a compositional API this is basically a
-				solved problem. */}
-				<script src="script.js"></script>
-			</body>
-		</html>
-	)
-}
 
 function index(path: string) {
 	return path === "/" ? "index" : path
 }
 
+// TODO: Should we just have a client and server folder?
 ;(() => {
 	Object.keys(routes).forEach(key => {
 		const doc = `<!DOCTYPE html>${ReactDOMServer.renderToString(
@@ -51,9 +17,10 @@ function index(path: string) {
 				<Document route={routes[key]} />
 			</StaticRouter>,
 		)}`
+		// TODO: Do we want to generate to public or somewhere else?
 		fs.writeFile(`public/${index(key)}.html`, doc, err => {
 			if (err) {
-				throw new Error("ssr: an unexpected error occurred: " + err)
+				throw new Error("ssg.tsx: an unexpected error occurred: " + err)
 			}
 		})
 	})
